abeebd9e8477f74d53ae3726b606c3fe79831e95
julyamba <jyamz7690@gmail.com>
2024-08-30 12:38:29 +0800

    update: day 3

---
+++ app/Models/Job.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Tag;
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];  
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tag(string $name)
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+        $this->tags()->attach($tag);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
